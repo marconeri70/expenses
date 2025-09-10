4f1a54dd600eb1d15ae08cd4d082992d7edfc590
marconeri70 <marconeri70@gmail.com>
2025-09-10 18:12:13 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,12 @@
-const CACHE = 'spese-cache-v1';
+const CACHE = 'spese-cache-v2';
 const ASSETS = [
   './',
   './index.html',
   './style.css',
   './app.js',
-  './manifest.webmanifest'
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
 ];
 
 self.addEventListener('install', (e) => {
